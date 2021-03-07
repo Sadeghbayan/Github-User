826b7f7b105427f51c511ee5791e8b0d860b1c33
Soheil Sadeghbayan <liam@ramsalt.com>
2021-03-07 17:59:19 +0330

    define some new routes
    delete extra views

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,22 +5,31 @@ const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
     name: "Home",
-    component: Home
+    component: Home,
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
+    path: "/:user_name/:repo_name",
+    name: "Repository",
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
-  }
+      import(/* webpackChunkName: "repository" */ "../views/Repository.vue"),
+  },
+  {
+    path: "/request-form",
+    name: "Search",
+    component: () =>
+      import(/* webpackChunkName: "search" */ "../views/Search.vue"),
+  },
+  {
+    path: "/github-user",
+    name: "User Information",
+    component: () =>
+      import(/* webpackChunkName: "github-user" */ "../views/Github-user.vue"),
+  },
 ];
 
 const router = createRouter({
   history: createWebHashHistory(),
-  routes
+  routes,
 });
 
 export default router;
