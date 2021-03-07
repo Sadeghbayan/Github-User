082045c50bdfaae85865d8046c918f0b58a82664
Soheil Sadeghbayan <liam@ramsalt.com>
2021-03-07 18:00:32 +0330

    define textfields componenet with page validation

---
+++ src/components/InputText.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="input-text__container">
+    <label :for="id">
+      {{ label }}
+    </label>
+    <input
+      :id="id"
+      :name="name"
+      :type="type"
+      class="input-text"
+      @input="handleInput"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    id: {
+      type: String,
+      required: true
+    },
+    label: {
+      type: String,
+      required: true
+    },
+    name: {
+      type: String,
+      required: true
+    },
+    type: {
+      type: String,
+      required: true
+    },
+    value: {
+      type: String,
+      required: false,
+      default: ""
+    }
+  },
+  data() {
+    return {
+      inputValue: this.value
+    };
+  },
+  methods: {
+    handleInput(e) {
+      this.inputValue = e.target.value;
+      this.$emit("input", this.inputValue);
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+.input-text__container {
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 1.25rem;
+}
+
+label {
+  font-weight: $font-weight-light;
+  font-size: $font-size-small;
+  margin-bottom: 5px;
+}
+
+.input-text {
+  width: 300px;
+  height: 35px;
+  padding: 0 0.5rem;
+  border: 1px solid $gray;
+  caret-color: $dark-blue;
+  color: $dark-blue;
+  transition: border-color 0.3s ease-in-out;
+
+  &:focus {
+    outline: none;
+    box-shadow: 0 0 0 3px $dark-gray;
+  }
+}
+</style>
---
+++ src/components/pageControl.vue
@@ -0,0 +1,83 @@
+<template>
+  <nav class="page-control__container">
+    <button
+      v-if="previousStep"
+      :disabled="previousStep.isDisabled"
+      class="main-cta page-control__previous-step"
+      @click="previousStep.action"
+    >
+      {{ previousStep.label }}
+    </button>
+    <button
+      v-if="nextStep"
+      :disabled="nextStep.isDisabled"
+      class="main-cta page-control__next-step"
+      @click="nextStep.action"
+    >
+      {{ nextStep.label }}
+    </button>
+  </nav>
+</template>
+
+<script>
+import pageValidator from "@/utils/pageValidator";
+
+export default {
+  props: {
+    nextStep: {
+      type: Object,
+      required: false,
+      default: null,
+      validator: pageValidator
+    },
+    previousStep: {
+      type: Object,
+      required: false,
+      default: null,
+      validator: pageValidator
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+.page-control__container {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  margin-top: auto;
+}
+
+.page-control__previous-step {
+  position: relative;
+  margin-right: auto;
+  padding-left: 1.5rem;
+
+  &::after {
+    position: absolute;
+    left: 0.5rem;
+    top: 50%;
+    width: 0.75rem;
+    height: 0.75rem;
+    content: " ";
+    transform: scaleX(-1) translateY(calc(-50% + 1px));
+  }
+}
+
+.steps-control__next-step {
+  position: relative;
+  margin-left: auto;
+  padding-right: 1.5rem;
+
+  &::after {
+    position: absolute;
+    right: 0.5rem;
+    top: 50%;
+    width: 0.75rem;
+    height: 0.75rem;
+    content: " ";
+    transform: translateY(calc(-50% + 1px));
+  }
+}
+</style>
