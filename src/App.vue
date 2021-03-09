2b00d0da1c5c092f6cb4f9002aa4cb6d225cad19
Soheil Sadeghbayan <liam@ramsalt.com>
2021-03-10 01:32:39 +0330

    remove extra header and footer

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,43 @@
 <template>
-  <header class="header">
-    <div class="header__inner container">
-      <router-link to="/">
-        <img alt="Homeday logo" src="./assets/logo.svg" class="header__logo" />
-      </router-link>
-      <h1>Github Repo</h1>
-    </div>
-  </header>
-  <router-view />
-  <footer class="footer">
-    <div class="footer__inner container">
-      <p>© 2021 Homeday assignment</p>
-    </div>
-  </footer>
+  <Header />
+  <section class="main">
+    <router-view v-slot="{ Component }">
+      <Suspense timeout="0">
+        <template #default>
+          <component :is="Component" />
+        </template>
+        <template #fallback>
+          <span class="loader"></span>
+        </template>
+      </Suspense>
+    </router-view>
+  </section>
+  <Footer />
 </template>
 
+<script lang="ts">
+import { defineComponent } from "vue";
+import { provideStore } from "@/store/store";
+import Header from "@/components/Header.vue";
+import Footer from "@/components/Footer.vue";
+export default defineComponent({
+  name: "App",
+  components: {
+    Header,
+    Footer
+  },
+  setup() {
+    //make our store globally available
+    provideStore();
+  }
+});
+</script>
+
 <style lang="scss">
-.header {
-  background: $white;
-  padding: 16px 0;
-  height: 40px;
-  display: flex;
-  flex-flow: row nowrap;
-  align-items: center;
-  color: $dark-gray;
-  &__inner {
-    display: flex;
-    flex-direction: row;
-  }
+@import "@/assets/styles/main";
 
-  &__logo {
-    width: 50px;
-    height: 50px;
-    margin-right: 10px;
-  }
-
-  h1 {
-    margin: 0;
-    align-self: center;
-    font-size: $font-size-default;
-    font-weight: $font-weight-bold;
-  }
-}
-//TODO: make a mixin for that so header and footer can reuse it
-.footer {
-  background: $white;
-  padding: 16px 0;
-  height: 40px;
+section.main {
   display: flex;
-  flex-flow: row nowrap;
-  align-items: center;
-  color: $dark-gray;
-  font-size: $font-size-small;
+  flex: 1 0 auto;
 }
 </style>
