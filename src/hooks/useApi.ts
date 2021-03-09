d5f6ab52b08b00e1c6b675a0866ae73c3f7a70db
Soheil Sadeghbayan <liam@ramsalt.com>
2021-03-10 01:39:41 +0330

    error handling in useApi hooks

--- src/hooks/useApi.ts
+++ src/hooks/useApi.ts
@@ -3,6 +3,7 @@ import { Ref, ref } from "vue";
 export type ApiRequest = () => Promise<void>;
 export interface UsableAPI<T> {
   response: Ref<T | undefined>;
+  error: any;
   request: ApiRequest;
 }
 
@@ -11,10 +12,14 @@ export default function useApi<T>(
   options?: RequestInit
 ): UsableAPI<T> {
   const response: Ref<T | undefined> = ref();
+  const error = ref(false);
   const request: ApiRequest = async () => {
-    const res = await fetch(url, options);
+    const res: any = await fetch(url, options);
+    if (!res.ok) {
+      error.value = res;
+    }
     const data = await res.json();
     response.value = data;
   };
-  return { response, request };
+  return { response, request, error };
 }
