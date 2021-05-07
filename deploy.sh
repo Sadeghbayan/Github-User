3b85ebfb1f168ecb10e4b20356725b0b964873fd
Soheil Sadeghbayan <liam@ramsalt.com>
2021-05-08 02:13:59 +0430

    setup github pages

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:Sadeghbayan/Github-User.git main:gh-pages
+git push -f git@github.com:Sadeghbayan/Github-User.git master:gh-pages
 cd -
